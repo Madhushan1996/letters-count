40b28d12e7acb4aad1ffdc5b5de8ae38946c13fe
Sumudu Madhushan <sumudumadhushan5@gmail.com>
2022-05-09 21:19:40 +0530

    export to csv

--- src/component/InputBox/InputBox.jsx
+++ src/component/InputBox/InputBox.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { CSVLink } from "react-csv";
 import "./InputBox.css";
 import { TextField, Button } from "@material-ui/core";
 import { calculator } from "../../utils";
 
+const csvHeaders = [
+  { label: "Name", key: "name" },
+  { label: "Characters", key: "length" },
+  { label: "Frequency", key: "count" },
+];
+
 const InputBox = ({ setResponse }) => {
   const [inputValue, setInputValue] = useState("");
+  const [outPut, setOutPut] = useState("");
   return (
     <div className="input">
       <h1>Input Your String</h1>
@@ -32,6 +40,7 @@ const InputBox = ({ setResponse }) => {
           onClick={async (e) => {
             const lettersArr = calculator(inputValue);
             setResponse(lettersArr);
+            setOutPut(lettersArr);
           }}
         >
           Enter
@@ -52,6 +61,22 @@ const InputBox = ({ setResponse }) => {
         >
           Clear
         </Button>
+
+        <CSVLink headers={csvHeaders} data={outPut} filename="letters_count">
+          <Button
+            type="button"
+            style={{
+              width: 200,
+              backgroundColor: "aquamarine",
+              textAlign: "center",
+              marginTop: 10,
+              marginLeft: 10,
+            }}
+            onClick={() => {}}
+          >
+            Export To CSV
+          </Button>
+        </CSVLink>
       </div>
     </div>
   );
